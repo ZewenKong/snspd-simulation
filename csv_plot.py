abd8d1fa3ac35980d2e8ab19cd2d55e01ca7c4ab
ZewenKong <zewen.kong@outlook.com>
2024-07-08 14:56:20 +0800

    240708

--- csv_plot.py
+++ csv_plot.py
@@ -1,5 +1,4 @@
 import tkinter as tk
-from tkinter import filedialog
 from matplotlib.figure import Figure
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 import pandas as pd
@@ -8,22 +7,34 @@ class CSVPlot:
     def __init__(self, root):
         self.root = root
 
-        # Scrollbars
-        self.lk_scrollbar = tk.Scale(root, from_=0, to=100, orient=tk.HORIZONTAL, label="Lk", command=self.update_plot)
-        self.lk_scrollbar.pack(fill=tk.X)
-
-        self.rl_scrollbar = tk.Scale(root, from_=0, to=100, orient=tk.HORIZONTAL, label="Rl", command=self.update_plot)
-        self.rl_scrollbar.pack(fill=tk.X)
-
-        # Matplotlib figure and canvas
-        self.figure = Figure(figsize=(6, 4), dpi=100)
+        # Set Matplotlib figure and canvas
+        self.figure = Figure(figsize=(4, 4), dpi=100)
         self.ax = self.figure.add_subplot(111)
         self.canvas = FigureCanvasTkAgg(self.figure, root)
         self.canvas.get_tk_widget().pack()
+     
+        # Scrollbars
+        self.lk_scrollbar = tk.Scale(root, 
+                                     from_=0,  # start value
+                                     to=100,  # end value
+                                     orient=tk.HORIZONTAL,  # horizental scrollbar
+                                     label="Lk",  # label Lk
+                                     command=self.update_plot)  # update the plot
+        
+        self.rl_scrollbar = tk.Scale(root, 
+                                     from_=0, 
+                                     to=100, 
+                                     orient=tk.HORIZONTAL, 
+                                     label="Rl", 
+                                     command=self.update_plot)
+        
+        # Fill the available width of the root window
+        self.lk_scrollbar.pack(fill=tk.X)  
+        self.rl_scrollbar.pack(fill=tk.X)
 
         self.data = None
-        self.unique_lk_values = []
-        self.unique_rl_values = []
+        self.unique_lk_values = []  # save unique Lk value
+        self.unique_rl_values = []  # save unique Rl value
         self.max_x_value = 0
         self.max_y_value = 0
 
@@ -34,14 +45,18 @@ class CSVPlot:
     def load_csv(self):
         file_path = self.csv_path
         self.data = pd.read_csv(file_path)
-        self.unique_lk_values = self.data['Lk'].unique()
+
+        self.unique_lk_values = self.data['Lk'].unique()  # take the unique value of Lk
         self.unique_rl_values = self.data['Rl'].unique()
         self.max_x_value = self.data['x'].max()
         self.max_y_value = self.data['y'].max()
+
         self.lk_scrollbar.config(to=len(self.unique_lk_values) - 1)
         self.lk_scrollbar.set(0)
+
         self.rl_scrollbar.config(to=len(self.unique_rl_values) - 1)
         self.rl_scrollbar.set(0)
+
         self.update_plot()
 
     def update_plot(self, *args):
@@ -54,14 +69,21 @@ class CSVPlot:
             filtered_data = self.data[(self.data['Lk'] == selected_lk) & (self.data['Rl'] == selected_rl)]
             self.ax.clear()
 
-            self.ax.plot(filtered_data['x'], filtered_data['y'], label=f"Lk={selected_lk}, Rl={selected_rl}")
+            self.ax.plot(filtered_data['x'], 
+                         filtered_data['y'], 
+                         label=f"Lk={selected_lk}, Rl={selected_rl}",
+                         linewidth='0.75')
 
             self.ax.legend()
-            self.ax.set_title(f"Plot for Lk={selected_lk}, Rl={selected_rl}")
-            self.ax.set_xlabel('x')
-            self.ax.set_ylabel('y')
+            
+            self.ax.set_xlabel('Time (seconds)', fontsize='small')
+            self.ax.set_ylabel('Voltage', fontsize='small')
+
             self.ax.set_xlim(0, self.max_x_value)
             self.ax.set_ylim(0, self.max_y_value)
+            self.ax.tick_params(axis='both', which='major', labelsize=8)
+            self.ax.xaxis.get_offset_text().set_fontsize(8) 
+
             self.ax.set_yticklabels([])
             self.canvas.draw()
 
--- main.py
+++ main.py
@@ -19,7 +19,7 @@ def main():
     plt.figure()
     # examine_inductance.ExamineInductance(asc_path, net_path, output_path, csv_output_path)
     # examine_resistance.ExamineResistance(asc_path, net_path, output_path, csv_output_path)
-    # examine.Examine(asc_path, net_path, output_path, csv_output_path)
+    examine.Examine(asc_path, net_path, output_path, csv_output_path)
 
     root = tk.Tk()
     app = CSVPlot(root)
